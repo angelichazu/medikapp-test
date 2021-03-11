2cf5e367a72c54e1bc8aeffb24bcd079a698eb23
angelichazu <facquer98@gmail.com>
2021-03-11 11:23:37 -0500

    Creacion de los metodos de listado

--- app.js
+++ app.js
@@ -16,19 +16,49 @@ const connection = mysql.createConnection({
     user: 'angel',
     password: 'angel4317',
     database: 'Medikapp'
-  });
+});
 
-
-// Rutas
+// RUTA
 app.get('/', (req, res) => {
-    res.send('CRUD Paciente');
+    res.send('Eiteck Back - CRUD PACIENTE \n' +
+            'Metodos Disponibles: \n'+
+            '/listarDoc  | Obtenemos los doctores disponibles \n' +
+            '/listar   | Obtenemos todos los pacientes');
 });
 
-// Verificar 
+// LISTAR DOCTORES
+app.get('/listarDoc', (req, res) => {
+    const sql = 'SELECT * FROM doctor';
 
+    connection.query(sql, (error, results) => {
+        if (error) throw error;
+        if (results.length > 0) {
+            res.json(results);
+        } else {
+            res.send('No hay resultados');
+        }
+    });
+});
+
+// LISTAR PACIENTES
+app.get('/listar', (req, res) => {
+    const sql = 'SELECT * FROM paciente';
+
+    connection.query(sql, (error, results) => {
+        if (error) throw error;
+        if (results.length > 0) {
+            res.json(results);
+        } else {
+            res.send('No hay resultados');
+        }
+    });
+});
+
+
+// VERIFICAR CONEXION
 connection.connect(error => {
-    if(error) throw error;
-    console.log('Base de datos funcionando');
+    if (error) throw error;
+    console.log('Base dato corriendo sin problemas.');
 });
 
-app.listen(PORT, ()=> console.log ('Server funcionando en el puerto: ${PORT}'));
+app.listen(PORT, () => console.log(`Server levantado en el puerto ${PORT}`));
